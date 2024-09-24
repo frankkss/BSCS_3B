ecc04f406d6ce3ff84ec48975857c82d621af25a
Divino Franco R. Aurellano <115411774+frankkss@users.noreply.github.com>
2024-09-24 06:24:56 +0000

    added  archi PS2

--- Archi_%_Org/PS2.asm
+++ Archi_%_Org/PS2.asm
@@ -1,23 +1,24 @@
 section .data
-    ;Prompting user 
-    prompt_name db 'Hi! Whats is your name?: ', 0
-    prompt_age db 'How old are you?: ', 0
-    prompt_address db 'Where do you live?: ', 0
-    input_format_name db '%s', 0
-    input_format_age db '%s', 0
-    input_format_address db '%s', 0
-    message db 'Nice to meet you, %s, %s, who lives at %s', 10, 0  ; Define the message string
+; Prompt user and display messages
+prompt_name db 'Hi! What is your name? ', 0
+prompt_age db 'How old are you? ', 0
+prompt_address db 'Where do you live? ', 0
+input_format_name db '%s', 0
+input_format_age db '%d', 0 ; Change format specifier to integer
+input_format_address db '%[^\n]', 0 ; Read until newline
+message db 'Nice to meet you, %s, %s, who lives at %s', 10, 0 ; Adjust the message string
 
 section .bss
-    userName resb 100    ; Buffer to store user input
-    userAge resd 1
-    userAddress resb 100
+userName resb 100 ; Buffer to store user input
+userAge resd 1 ; Reserve space for an integer
+userAddress resb 100
 
 section .text
-    global _main
-    extern _printf
-    extern _scanf
-    
+global _main
+extern _printf
+extern _scanf
+extern _getchar
+
 _main:
     ; Display a name prompt
     push prompt_name
@@ -29,7 +30,7 @@ _main:
     push input_format_name
     call _scanf
     add esp, 8
-    
+
     ; Display age prompt
     push prompt_age
     call _printf
@@ -41,23 +42,29 @@ _main:
     call _scanf
     add esp, 8
 
+    ; Consume the newline character left by scanf
+    call _getchar
+
     ; Display address prompt
     push prompt_address
     call _printf
     add esp, 4
 
-    ; Read user address
-    push userAddress
-    push input_format_address
-    call _scanf
-    add esp, 8
-
-    ; Display the entered text
-    push userAddress
-    push userAge
+    ; Read user address 
+    push userAddress 
+    push input_format_address 
+    call _scanf 
+    add esp, 8 
+    
+    ; Display the message 
+    push userAddress 
+    push userAge 
     push userName
-    push message
-    call _printf
-    add esp, 16
 
-    ret
+    ; Display the message 
+    push userAddress 
+    push userAge 
+    push userName
+    push message 
+    call _printf 
+    add esp, 16
