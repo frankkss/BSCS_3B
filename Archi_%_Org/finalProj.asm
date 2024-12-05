def041f56f173a7c408452ec53bc85aacc2aab3f
Divino Franco R. Aurellano <115411774+frankkss@users.noreply.github.com>
2024-12-05 12:30:55 +0000

    modify final project

--- Archi_%_Org/finalProj.asm
+++ Archi_%_Org/finalProj.asm
@@ -28,6 +28,7 @@ section .data
     err2 db '|     Input should only be between -1000 to 1000. Please enter again a valid input. |', 10, 0
     err3 db '|     You can not shift by more bits than 16-bits.                                  |', 10, 0
     err4 db '|     You can not shift by negative bits.                                           |', 10, 0
+    err5 db "|     Please enter a valid integer.                                                 |", 10, 0
     ; Titles for each operation
     ti1 db  "|===================================================================================|", 10, \
             "|                         A N D  O P E R A T I O N  T O O L                         |", 10, \
@@ -64,12 +65,14 @@ section .bss
     val2 resw 1
     userinput resb 100
     result_v resw 1
+    
 
 section .text
     global _main
     extern _printf
     extern _scanf
-
+    extern _getchar
+    
 _main:
     ; Print program introduction
     push pro1
@@ -93,6 +96,8 @@ main_loop:
     call _scanf
     add esp, 4
 
+    call clear_input_buffer
+
     ; Check user choice and jump to corresponding operation
     mov eax, [choice]
     cmp eax, 1
@@ -108,6 +113,14 @@ main_loop:
     cmp eax, 6
     jg errormain
 
+
+
+clear_input_buffer:
+    call _getchar
+    cmp al, 10    ; Check for newline (Enter key)
+    jne errormain   ; Repeat until newline is found
+    
+
 errormain:
     ; Handle invalid choice
     push err1
@@ -115,9 +128,41 @@ errormain:
     add esp, 4
     jmp main_loop
 
+and_clear_input_buffer:
+    call _getchar
+    cmp al, 10    ; Check for newline (Enter key)
+    jne error_and1   ; Repeat until newline is found
+    ret
+
+or_clear_input_buffer:
+    call _getchar
+    cmp al, 10    ; Check for newline (Enter key)
+    jne error_or1   ; Repeat until newline is found
+    ret
+
+xor_clear_input_buffer:
+    call _getchar
+    cmp al, 10    ; Check for newline (Enter key)
+    jne error_xor1   ; Repeat until newline is found
+    ret
+
+left_shift_clear_input_buffer:
+    call _getchar
+    cmp al, 10    ; Check for newline (Enter key)
+    jne error_ls4   ; Repeat until newline is found
+    ret
+
+
 error_and:
     ; Handle invalid input for AND operation
     push err2
+    call _printf
+    add esp, 4
+    jmp and_operation
+
+error_and1:
+    ; Handle invalid input for AND operation
+    push err5
     call _printf
     add esp, 4
     jmp and_operation
@@ -129,9 +174,23 @@ error_or:
     add esp, 4
     jmp or_operation
 
+error_or1:
+    ; Handle invalid input for OR operation
+    push err5
+    call _printf
+    add esp, 4
+    jmp or_operation
+
 error_xor:
     ; Handle invalid input for XOR operation
     push err2
+    call _printf
+    add esp, 4
+    jmp xor_operation
+
+error_xor1:
+    ; Handle invalid input for XOR operation
+    push err5
     call _printf
     add esp, 4
     jmp xor_operation
@@ -157,6 +216,13 @@ error_ls3:
     add esp, 4
     jmp left_shift_operation
 
+error_ls4:
+    ; Handle invalid input for left shift operation
+    push err5
+    call _printf
+    add esp, 4
+    jmp left_shift_operation
+
 and_operation:
     ; Perform AND operation
     push ti1
@@ -176,6 +242,8 @@ and_operation:
     jl error_and
     add esp, 4
 
+    call and_clear_input_buffer
+
     push prompt2
     call _printf
     add esp, 4
@@ -189,6 +257,8 @@ and_operation:
     cmp eax, -1000
     jl error_and
     add esp,4
+    
+    call and_clear_input_buffer
 
     mov eax, [num1]
     and eax, [num2]
@@ -219,6 +289,8 @@ or_operation:
     jl error_or
     add esp, 4
 
+    call or_clear_input_buffer
+
     push prompt2
     call _printf
     add esp, 4
@@ -232,6 +304,8 @@ or_operation:
     cmp eax, -1000
     jl error_or
     add esp,4
+
+    call or_clear_input_buffer
 
     mov eax, [num1]
     or eax, [num2]
@@ -262,6 +336,8 @@ xor_operation:
     jl error_xor
     add esp, 8
 
+    call xor_clear_input_buffer
+
     push prompt2
     call _printf
     add esp, 4
@@ -275,6 +351,8 @@ xor_operation:
     cmp eax, -1000
     jl error_xor
     add esp,8
+
+    call xor_clear_input_buffer
 
     mov eax, [num1]
     xor eax, [num2]
@@ -305,6 +383,8 @@ left_shift_operation:
     jl error_ls1
     add esp, 8
 
+    call left_shift_clear_input_buffer
+
     push prompt4
     call _printf
     add esp, 4
@@ -318,6 +398,8 @@ left_shift_operation:
     cmp eax, 0
     jl error_ls3
     add esp,8
+
+    call left_shift_clear_input_buffer
 
     mov eax, [num1]
     mov cl, [num2]
